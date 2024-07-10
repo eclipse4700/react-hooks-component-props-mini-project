4eacd5ebea6f949d17fefab3838c69f59f6a0904
Isaiah Kantor <isaiahkantor23@gmail.com>
2024-07-10 14:34:19 -0400

    Fixed Today

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,9 @@ import React from "react";
 
 function Header({name}) {
     return (
-        <Header>
-            {name}
-        </Header>
+        <header>
+            <h1>{name}</h1>
+        </header>
     )
 }
 
